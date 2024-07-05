3ce7a51bd1e54098e2d4ec0a2292cde891ad3a6c
clementtalleu <clement.talleu@gmail.com>
2024-07-05 15:00:29 +0200

    fix migration

--- api/migrations/Version20240703135318.php
+++ api/migrations/Version20240703135318.php
@@ -17,7 +17,7 @@ final class Version20240703135318 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE SEQUENCE repairer_city_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE repairer_city (id INT NOT NULL, repairer_id INT NOT NULL, name VARCHAR(255) NOT NULL, postcode VARCHAR(255) NOT NULL, lat VARCHAR(255) NOT NULL, lon VARCHAR(255) NOT NULL, gps_point geography(GEOMETRY, 4326) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE repairer_city (id INT NOT NULL, repairer_id INT NOT NULL, name VARCHAR(255) NOT NULL, postcode VARCHAR(255) NOT NULL, latitude VARCHAR(255) NOT NULL, longitude VARCHAR(255) NOT NULL, gps_point geography(GEOMETRY, 4326) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_92A7424047C5DFEE ON repairer_city (repairer_id)');
         $this->addSql('ALTER TABLE repairer_city ADD CONSTRAINT FK_92A7424047C5DFEE FOREIGN KEY (repairer_id) REFERENCES repairer (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
