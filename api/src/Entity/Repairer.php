ba5c1b2e4bc290d0b6f8592c2d270a8173174c40
Davy Beauzil <davy-beauzil@MacBook-Pro-de-Davy.local>
2024-08-07 14:25:49 +0200

    fix for php-cs-fixer

--- api/src/Entity/Repairer.php
+++ api/src/Entity/Repairer.php
@@ -35,7 +35,6 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Jsor\Doctrine\PostGIS\Types\PostGISType;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
