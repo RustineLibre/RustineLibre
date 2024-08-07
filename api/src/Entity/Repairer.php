9ad62d84005d4fe7c4af56ba1ccfe3077ad0d6c2
Davy Beauzil <davy-beauzil@MacBook-Pro-de-Davy.local>
2024-08-07 14:06:05 +0200

    fix for ci

--- api/src/Entity/Repairer.php
+++ api/src/Entity/Repairer.php
@@ -119,7 +119,7 @@ class Repairer
     #[Groups([self::REPAIRER_READ, self::REPAIRER_COLLECTION_READ, Appointment::APPOINTMENT_READ, Discussion::DISCUSSION_READ, User::USER_READ, RepairerEmployee::EMPLOYEE_READ])]
     public ?int $id = null;
 
-    #[ORM\ManyToOne(targetEntity: User::class, cascade: ['persist'])]
+    #[ORM\ManyToOne(targetEntity: User::class, cascade: ['persist'], inversedBy: 'repairers')]
     #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
     #[Groups([self::REPAIRER_READ, self::REPAIRER_WRITE, self::REPAIRER_COLLECTION_READ])]
     public ?User $owner = null;
