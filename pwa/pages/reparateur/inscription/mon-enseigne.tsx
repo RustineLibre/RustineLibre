913cf6026690bb23690ce245c396116860f17b64
Raphael Marchese <marchese.raphael@gmail.com>
2024-09-02 11:09:24 +0200

    fix CI

--- pwa/pages/reparateur/inscription/mon-enseigne.tsx
+++ pwa/pages/reparateur/inscription/mon-enseigne.tsx
@@ -1,6 +1,6 @@
 import {NextPageWithLayout} from '@interfaces/NextPageWithLayout';
 import RepairerRegistrationLayout from '@components/layout/RepairerRegistrationLayout';
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useEffect, useState} from 'react';
 import {RegistrationTunnelWorkshop} from '@components/repairers/registration/RegistrationTunnelWorkshop';
 import {GetStaticProps} from 'next';
 import {bikeTypeResource} from '@resources/bikeTypeResource';
@@ -15,6 +15,30 @@ type RepairerRegistrationProps = {
 const RepairerWorkshopRegistration: NextPageWithLayout<
   RepairerRegistrationProps
 > = ({bikeTypesFetched = [], repairerTypesFetched = []}) => {
+  const [bikeTypes, setBikeTypes] = useState<BikeType[]>(bikeTypesFetched);
+  const [repairerTypes, setRepairerTypes] =
+    useState<RepairerType[]>(repairerTypesFetched);
+  const fetchRepairerTypes = async () => {
+    const responseRepairerTypes = await repairerTypeResource.getAll(false);
+    setRepairerTypes(responseRepairerTypes['hydra:member']);
+  };
+
+  useEffect(() => {
+    if (repairerTypes.length === 0) {
+      fetchRepairerTypes();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const fetchBikeTypes = async () => {
+    const responseBikeTypes = await bikeTypeResource.getAll(false);
+    setBikeTypes(responseBikeTypes['hydra:member']);
+  };
+
+  useEffect(() => {
+    if (bikeTypes.length === 0) {
+      fetchBikeTypes();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
   return (
     <>
       <RegistrationTunnelWorkshop
@@ -25,7 +49,7 @@ const RepairerWorkshopRegistration: NextPageWithLayout<
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+/*export const getStaticProps: GetStaticProps = async () => {
   const bikeTypesCollection = await bikeTypeResource.getAll(false);
   const bikeTypesFetched = bikeTypesCollection['hydra:member'];
 
@@ -39,7 +63,7 @@ export const getStaticProps: GetStaticProps = async () => {
     },
     revalidate: 10,
   };
-};
+};*/
 
 RepairerWorkshopRegistration.getLayout = (page: ReactElement) => {
   // here the best is to create a RepairerRegistrationLayout and put the context and the text "Devenir réparateur" in it
