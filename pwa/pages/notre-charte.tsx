e219cbf84856bc82142238950475c131548da18f
Raphael Marchese <marchese.raphael@gmail.com>
2024-09-24 15:14:31 +0200

    Add new version of the charter

--- pwa/pages/notre-charte.tsx
+++ pwa/pages/notre-charte.tsx
@@ -31,32 +31,32 @@ const OurCharter: NextPageWithLayout = () => {
               </Typography>
               <Typography component="p">
                 En 2022, des réparateurs du Nord Pas-de-Calais se sont réunis,
-                sous l’impulsion d’un projet européen soutenu par l’Apes, pour
-                développer un outil numérique respectueux de l’humain, dans une
-                logique de Commun.
+                sous l’impulsion d’un projet européen soutenu par l’Apes
+                (Acteurs pour une économie solidaire), pour développer un outil
+                numérique respectueux de l’humain, dans une logique de Commun.
               </Typography>
               <Typography component="p">
                 Le collectif est constitué d’associations d’aide à la réparation
-                , de réparateurs exerçants seuls, et d’ateliers de réparation
-                regroupant plusieurs réparateurs. Nos fonctionnements sont
+                , de réparateur·ice·s exerçants seul·e·s, et d’ateliers de réparation
+                regroupant plusieurs réparateur·ice·s. Nos fonctionnements sont
                 différents mais nos objectifs sont les mêmes : mutualisation,
                 transition écologique et sociétale, coopération... Nous mettons
                 notre expérience et nos compétences au service des cyclistes.
               </Typography>
               <Typography component="p">
                 Nous proposons des solutions de réparation, du conseil, de la
-                formation, qui rendront les usagers du vélo de plus en plus
+                formation, qui rendront les usager·e·s du vélo de plus en plus
                 autonomes
               </Typography>
               <Typography component="p">
-                Cette présente charte expose nos engagements vis à vis des
-                utilisateurs de l’application et permettront à d’autres de se
+                Cette présente charte expose nos engagements vis-à-vis des
+                utilisateur·ice·s de l’application et permettront à d’autres de se
                 reconnaître et de nous rejoindre !
               </Typography>
             </Box>
             <Box>
               <Typography component="h3" fontWeight={600} color="primary">
-                1. Engagements professionnels
+                1. Engagements des ateliers de réparation
               </Typography>
               <List
                 sx={{
@@ -99,8 +99,62 @@ const OurCharter: NextPageWithLayout = () => {
             </Box>
             <Box>
               <Typography component="h3" fontWeight={600} color="primary">
-                2. Engagements sur les pratiques des différents acteurs de la
-                réparation
+                1 bis. Engagements des ateliers d'aide à la réparation
+              </Typography>
+              <List
+                sx={{
+                  listStyleType: 'disc',
+                  pl: 4,
+                  py: 0,
+                }}>
+                <ListItem
+                  sx={{
+                    display: 'list-item',
+                  }}>
+                  accueillir sans discrimination des cyclistes voulant apprendre
+                  à réparer eux-mêmes leurs vélos
+                </ListItem>
+                <ListItem
+                  sx={{
+                    display: 'list-item',
+                  }}>
+                  les ateliers associatifs s’engagent sur du conseil, de l'aide
+                  et des moyens pour réparer les vélos des adhérents, dans une
+                  démarche d’accompagnement et de transmission de savoir
+                </ListItem>
+                <ListItem
+                  sx={{
+                    display: 'list-item',
+                  }}>
+                  les réparateur.rice.s bénévoles et/ ou salariés font de leur
+                  mieux pour répondre aux demandes, remettre en état de
+                  fonctionnement les vélos, sans obligation de résultat, ni
+                  engager leur responsabilité sur des dommages éventuels suite à
+                  leur accompagnement
+                </ListItem>
+                <ListItem
+                  sx={{
+                    display: 'list-item',
+                  }}>
+                  de l'information relatives à la sécurité (attitudes,
+                  équipements, réparations) seront préconisées, sans pourtant
+                  engager la responsabilité de l'atelier suite à la
+                  participation à un atelier d'aide à la réparation
+                </ListItem>
+                <ListItem
+                  sx={{
+                    display: 'list-item',
+                  }}>
+                  proposer une charte et/ ou un règlement intérieur accessibles
+                  et transparents pour expliquer leur fonctionnement propre, qui
+                  doit être consulté et accepté
+                </ListItem>
+              </List>
+            </Box>
+            <Box>
+              <Typography component="h3" fontWeight={600} color="primary">
+                2. Engagements sur les pratiques des différent·e·s acteur·ice·s
+                de la réparation
               </Typography>
               <List
                 sx={{
@@ -133,7 +187,7 @@ const OurCharter: NextPageWithLayout = () => {
                     display: 'list-item',
                   }}>
                   mettre en valeur et développer les compétences de réparation
-                  des réparateurs et des usagers
+                  des réparateurs et des cyclistes
                 </ListItem>
                 <ListItem
                   sx={{
@@ -151,7 +205,7 @@ const OurCharter: NextPageWithLayout = () => {
             </Box>
             <Box>
               <Typography component="h3" fontWeight={600} color="primary">
-                3. Engagement dans le collectif de réparateurs
+                3. Engagement dans le collectif de réparateur·ice·s
               </Typography>
               <List
                 sx={{
