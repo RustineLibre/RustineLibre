a0d8d6f52bebb0d68ff7c3120c052dfa7982e7fc
Maxime <maxime.valin@les-tilleuls.coop>
2025-11-05 13:56:43 +0100

    Upgrade node (#133)
    * chore(deps): upgrade playwrite to work with recent distributionS
    * test(pwa): allows to use a different port with playwright
    * chore: upgrade Node.js and update package dependencies
    
    ---------
    
    Co-authored-by: Nicolas LAURENT <nicolas.laurent@les-tilleuls.coop>

--- pwa/playwright.config.ts
+++ pwa/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://localhost',
+    baseURL: 'https://localhost:' + (process.env.HTTPS_PORT || 443),
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
