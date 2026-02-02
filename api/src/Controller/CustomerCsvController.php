4e1782ad04dcea384e39f06ee77e606c57a8f564
Jérôme Tanghe <jerome.tanghe@les-tilleuls.coop>
2026-02-02 09:55:02 +0100

    fix regressions due to dependency upgrade

--- api/src/Controller/CustomerCsvController.php
+++ api/src/Controller/CustomerCsvController.php
@@ -11,7 +11,7 @@ use App\Repository\UserRepository;
 use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[AsController]
